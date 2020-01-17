8c8b5c0cd7a2648e7db9d86b8922537aebbc8b8b
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-01-17 18:51:29 +0000

    Add separators to Comment str

--- comment.py
+++ comment.py
@@ -6,7 +6,9 @@ class Comment(object):
         self.permalink = permalink
 
     def __str__(self) -> str:
-        return (f"author: {self.author}\n"
+        return (f"---\n"
+                f"author: {self.author}\n"
                 f"datetime: {self.datetime}\n"
                 f"permalink: {self.permalink}\n"
-                f"message: {self.message}")
+                f"message: {self.message}"
+                f"---\n")
