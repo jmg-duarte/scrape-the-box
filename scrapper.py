8b34df9f9f3bbf825e70f7fd1c942685b16a6eaf
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-01-17 18:51:51 +0000

    Add command line arg support

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,6 @@
 import requests
+import argparse
+
 from bs4 import BeautifulSoup
 
 from comment import Comment
@@ -6,15 +8,18 @@ from comment import Comment
 BASE_URL = "https://forum.hackthebox.eu/discussion/"
 
 
-def scrape(thread_id):
+def scrape_comments(thread_id):
     html = requests.get(f"{BASE_URL}{thread_id}").content
     soup = BeautifulSoup(html, 'html.parser')
     page_name = soup.find(class_="PageTitle").text
     print(page_name)
     last_page = int(soup.find(class_="LastPage").text)
+    comments = []
     for page_number in range(1, last_page + 1):
         page_url = f"{BASE_URL}{thread_id}/{page_name}/p{page_number}"
-        extract_page_comments(page_url)
+        page_comments = extract_page_comments(page_url)
+        comments.extend(page_comments)
+    return comments
 
 
 def extract_page_comments(page_url):
@@ -38,4 +43,26 @@ def extract_comment(c):
     return Comment(username, message, datetime, permalink)
 
 
-scrape(2427)
+def get_args():
+    parser = argparse.ArgumentParser(description="Description placeholder")
+    parser.add_argument('id', type=str, help="The target thread id")
+    parser.add_argument('-o', '--output', type=str, help="Output file", metavar='output', required=False)
+    args = parser.parse_args()
+    # parser.add_argument('-p', help="The thread page number, use for a single page")
+    return args
+
+
+def main():
+    args = get_args()
+    comments = scrape_comments(args.id)
+    if args.output is not None:
+        with open(args.output, "w") as output_file:
+            for comment in comments:
+                output_file.write(str(comment))
+        print(f"Written {len(comments)} comments to {args.output}")
+    else:
+        print(str(comments))
+
+
+if __name__ == '__main__':
+    main()
