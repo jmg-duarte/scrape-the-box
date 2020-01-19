3999221e81a54b818f52b2e9a49606932e38403c
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-01-19 21:59:26 +0000

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="scrape-the-box",
-    version="0.1.1",
+    version="0.1.2",
     pymodules=["stb"],
     include_package_data=True,
     install_required=["click", "bs4", "requests",],
