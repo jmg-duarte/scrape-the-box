7ce8882580b035901cb3231812f62a496df9e698
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-01-20 12:17:13 +0000

    Extract functions

--- stb/commands/fetch/frontpage.py
+++ stb/commands/fetch/frontpage.py
@@ -12,29 +12,23 @@ from stb.commands.fetch import io
 
 
 def scrape(output_file, all=False, fmt="text"):
-    frontpage = fetch_frontpage()
+    frontpage_soup = fetch_frontpage_soup()
     if all:
-        soup = BeautifulSoup(frontpage, "html.parser")
         last_page = io.get_last_page_number(soup)
         discussions = []
         for page in range(1, last_page + 1):
-            frontpage = fetch_frontpage(page)
-            discussions.extend(scrape_page_discussions(frontpage))
+            frontpage_soup = fetch_frontpage_soup(page)
+            discussions.extend(extract_discussions(frontpage_soup))
     else:
-        discussions = scrape_page_discussions(frontpage)
+        discussions = extract_discussions(frontpage_soup)
     io.write_file(discussions, output_file, fmt)
 
 
-def fetch_frontpage(page_number=1):
-    return io.fetch_page(f"{DISCUSSIONS_URL}/p{page_number}")
+def fetch_frontpage_soup(page_number=1):
+    return io.fetch_page_soup(f"{DISCUSSIONS_URL}/p{page_number}")
 
 
-def scrape_page_discussions(page):
-    soup = BeautifulSoup(page, "html.parser")
-    return get_discussions(soup)
-
-
-def get_discussions(soup):
+def extract_discussions(soup):
     discussions = []
     for item in soup.find_all(class_="ItemDiscussion"):
         discussions.append(Discussion.from_item(item))
