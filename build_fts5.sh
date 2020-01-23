2352e9b707884c166cfa3fcb7fa346dd039e0078
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-01-23 17:17:50 +0000

    Add SQLite FTS5 build script

---
+++ build_fts5.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+HOME_DIR="$(pwd)"
+OUTPUT_DIR="$1"
+
+SQLITE_NAME="SQLite-release"
+SQLITE_TGZ="$SQLITE_NAME.tgz" 
+
+wget -c "www.sqlite.org/src/tarball/SQLite-release.tgz?uuid=release" -O $SQLITE_TGZ
+
+tar -xzf $SQLITE_TGZ
+
+cd "$SQLITE_NAME/" || exit
+
+./configure && make "fts5.c"
+
+cd "$HOME_DIR" || exit
+
+case "$(uname)" in
+    "Linux")
+        gcc -g -fPIC -shared "$SQLITE_NAME/fts5.c" -o "$OUTPUT_DIR/fts5.o"
+    ;;
+    "Darwin")
+        gcc -g -fPIC -shared "$SQLITE_NAME/fts5.c" -o "$OUTPUT_DIR/fts5.dylib"
+    ;;
+    *) exit
+esac
