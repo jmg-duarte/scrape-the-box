35bd9b6a65528d4d0f359fd04f921c0d30863818
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-01-24 18:39:51 +0000

    Prepend build commands with /usr/bin

--- build_fts5.sh
+++ build_fts5.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 HOME_DIR="$(pwd)"
 OUTPUT_DIR="$1"
@@ -6,22 +6,22 @@ OUTPUT_DIR="$1"
 SQLITE_NAME="SQLite-release"
 SQLITE_TGZ="$SQLITE_NAME.tgz" 
 
-wget -c "www.sqlite.org/src/tarball/SQLite-release.tgz?uuid=release" -O $SQLITE_TGZ
+/usr/bin/wget -c "www.sqlite.org/src/tarball/SQLite-release.tgz?uuid=release" -O $SQLITE_TGZ
 
-tar -xzf $SQLITE_TGZ
+/usr/bin/tar -xzf $SQLITE_TGZ
 
 cd "$SQLITE_NAME/" || exit
 
-./configure && make "fts5.c"
+./configure && /usr/bin/make "fts5.c"
 
 cd "$HOME_DIR" || exit
 
 case "$(uname)" in
     "Linux")
-        gcc -g -fPIC -shared "$SQLITE_NAME/fts5.c" -o "$OUTPUT_DIR/fts5.so"
+        /usr/bin/gcc -g -fPIC -shared "$SQLITE_NAME/fts5.c" -o "$OUTPUT_DIR/fts5.so"
     ;;
     "Darwin")
-        gcc -g -fPIC -shared "$SQLITE_NAME/fts5.c" -o "$OUTPUT_DIR/fts5.dylib"
+        /usr/bin/gcc -g -fPIC -shared "$SQLITE_NAME/fts5.c" -o "$OUTPUT_DIR/fts5.dylib"
     ;;
     *) exit
 esac
