0f86add16a48690f0671442dd350950daff3c48f
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-01-27 18:47:59 +0000

    Update the setup script

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-from subprocess import check_call
-
-
-class PostInstallCommand(install):
-    def run(self):
-        check_call("sh build_fts5.sh .")
-        install.run(self)
 
 
 setup(
     name="scrape-the-box",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["click", "bs4", "requests",],
