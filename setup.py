8baa071a03a5f42ec5c0a4c40e174761edbc2d7f
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-01-27 18:12:18 +0000

    Correct setup arguments

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from subprocess import check_call
+
+
+class PostInstallCommand(install):
+    def run(self):
+        check_call("sh build_fts5.sh .")
+        install.run(self)
 
 setup(
     name="scrape-the-box",
     version="0.2.0",
-    pymodules=["stb"],
+    py_modules=["stb"],
+    packages=find_packages()
     include_package_data=True,
-    install_required=["click", "bs4", "requests",],
+    install_requires=["click", "bs4", "requests",],
     entry_points="""
         [console_scripts]
         stb=stb.cli:cli
     """,
+    zip_safe=False,
+    author="José Duarte",
+    author_email="jmg.duarte@campus.fct.unl.pt",
+    description="A CLI tool to scrape the Hack the Box forum",
+    keywords="scrape htb",
+    url="https://github.com/jmg-duarte/scrape-the-box",
+    project_urls={
+        "Source Code": "https://github.com/jmg-duarte/scrape-the-box",
+    }
 )
